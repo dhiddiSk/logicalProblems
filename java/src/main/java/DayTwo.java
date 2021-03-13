6648a9372259c4ad83c7bb6130a75ba470c2bfc1
saikrishna <saikrishna.4369@gmail.com>
2021-03-13 17:33:52 +0100

    update

--- java/src/main/java/DayTwo.java
+++ java/src/main/java/DayTwo.java
@@ -1,5 +1,4 @@
 /*
-
 Given an array of integers, return a new array such that each element at index 'i' of the new array is the product of all the numbers in the original array except the one at 'i'.
 
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
@@ -7,52 +6,53 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 */
 public class DayTwo {
-
-    // With Division
+    
+    //with division
     public int[] ModifyArrayWithDivision(int[] array) throws IllegalArgumentException {
-        // find the result of multiplication of all the elements.
-        int[] NewArray = new int[array.length];
+
         if (array.length <= 0) {
             throw new IllegalArgumentException("Invalid array passed");
         }
 
-        int temp = 1;
+        // find the result of multiplication of all the elements.
+        int[] NewArrayAfterDivisions = new int[array.length];
+        int valueAfterMultiplications = 1;
         for (int value : array) {
-            temp = temp * value;
+            valueAfterMultiplications = valueAfterMultiplications * value;
         }
-        //Now divide 'temp' with each element of the array
 
-        int tempTwo;
+
+        //Now divide 'valueAfterMultiplications' with each element of the array
+        int valueAfterDivision;
         for (int i = 0; i < array.length; i++) {
-            tempTwo = Math.abs(temp / array[i]);
-            NewArray[i] = tempTwo;
+            valueAfterDivision = Math.abs(valueAfterMultiplications / array[i]);
+            NewArrayAfterDivisions[i] = valueAfterDivision;
         }
-        return NewArray;
+        return NewArrayAfterDivisions;
     }
 
     // With multiplication
-    public int[] ModifyArrayWithMultiplication(int[] array) {
-        int[] NewArray = new int[array.length];
+    public int[] ModifyArrayWithMultiplication(int[] array) throws IllegalArgumentException{
+        int[] NewArrayAfterMultiplications = new int[array.length];
 
         if (array.length <= 0) {
             throw new IllegalArgumentException("Invalid array passed");
         }
 
-
         for (int i = 0; i < array.length; i++) {
-            int temp = 1;
+            int valueAfterMultiplication = 1;
             for (int j = 0; j < array.length; j++) {
 
                 // Here I need to exclude the multiplication of the element with index of i'th iteration.
                 if (j != i) {
-                    temp = temp * array[j];
+                    valueAfterMultiplication = valueAfterMultiplication * array[j];
                 }
                 // Now add that element to the new array.
-                NewArray[i] = temp;
+                NewArrayAfterMultiplications[i] = valueAfterMultiplication;
             }
         }
 
-        return NewArray;
+        return NewArrayAfterMultiplications;
 
     }
 }
