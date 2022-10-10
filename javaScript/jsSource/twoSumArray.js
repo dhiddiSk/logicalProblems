d4065b40a207bc322129f1ba3be747c8fce65292
saikrishna <saikrishna.4369@gmail.com>
2022-10-10 18:31:43 +0200

    modified function and added a small test

--- javaScript/jsSource/twoSumArray.js
+++ javaScript/jsSource/twoSumArray.js
@@ -1,24 +1,20 @@
 /*
-Task: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Task: Given an array of integers and an target integer, return indices of the two numbers from given array such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+points to note: a. You may assume that each input would have exactly one solution, b. you may not use the same element twice.
 
-You can return the answer in any order. 
+                c. You can return the answer in any order. 
 
 */
 
+const twoSum = function (integerArray, targetInteger) {
+  const numberObj = {};
 
-const twoSum = function (nums, target) {
-
-    const numberMap = new Map();
-
-    for (const [index, number] of Object.entries(nums)) {
-
-        if (numberMap.has(number)) return [numberMap.get(number), index];
-
-        numberMap.set(target - number, index);
-    }
-
+  for (const item of Object.entries(integerArray)) {
+    if (numberObj[item[1]])
+      return [Number(numberObj[item[1]]), Number(item[0])];
+    numberObj[targetInteger - item[1]] = item[0];
+  }
 };
-// test example
-console.log(twoSum([15, 7, 11, 2], 9));
+
+module.exports = twoSum;
