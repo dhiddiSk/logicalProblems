bb29364027ad0a0d9ff2c533c6f151501497eba4
saikrishna <saikrishna.4369@gmail.com>
2022-10-31 17:15:00 +0100

    converted from ts to js

---
+++ javaScript/jsSource/validateBalancedBrackets.ts
@@ -0,0 +1,63 @@
+'use strict'
+
+const validateBracketString = '{}{{}'
+
+enum brackets {
+  openBracket = '(',
+  closeBracket = ')',
+  openSquareBracket = '[',
+  closeSquareBracket = ']',
+  openFlowerBracket = '{',
+  closeFloweBracket = '}'
+
+}
+
+const arrayOpenbracketsQueue: string[] = []
+
+const validateBalancedBrackets = function (stringToValidate: string): any {
+  const stringLength: number = stringToValidate.length
+
+  for (let i = 0; i < stringLength; i++) {
+    if ((stringToValidate.charAt(i) === brackets.openBracket) || (stringToValidate.charAt(i) === brackets.openSquareBracket) || (stringToValidate.charAt(i) === brackets.openFlowerBracket)) {
+      arrayOpenbracketsQueue.push(stringToValidate.charAt(i))
+    } else if ((stringToValidate.charAt(i) === brackets.closeBracket) || (stringToValidate.charAt(i) === brackets.closeSquareBracket) || (stringToValidate.charAt(i) === brackets.closeFloweBracket)) {
+      switch (stringToValidate.charAt(i)) {
+        case ')' : if (arrayOpenbracketsQueue[arrayOpenbracketsQueue.length - 1] === '(') {
+          arrayOpenbracketsQueue.pop()
+          if ((arrayOpenbracketsQueue.length === 0) && (i === stringLength - 1)) {
+            return true
+          } else if ((arrayOpenbracketsQueue.length > 0) && (i === stringLength - 1)) {
+            return false
+          } else if (arrayOpenbracketsQueue.length > 0) {
+            continue
+          }
+        }
+          break
+        case '}' : if (arrayOpenbracketsQueue[arrayOpenbracketsQueue.length - 1] === '{') {
+          arrayOpenbracketsQueue.pop()
+          if ((arrayOpenbracketsQueue.length === 0) && (i === stringLength - 1)) {
+            return true
+          } else if ((arrayOpenbracketsQueue.length > 0) && (i === stringLength - 1)) {
+            return false
+          } else if (arrayOpenbracketsQueue.length > 0) {
+            continue
+          }
+        }
+          break
+        case ']' : if (arrayOpenbracketsQueue[arrayOpenbracketsQueue.length - 1] === '[') {
+          arrayOpenbracketsQueue.pop()
+          if ((arrayOpenbracketsQueue.length === 0) && (i === stringLength - 1)) {
+            return true
+          } else if ((arrayOpenbracketsQueue.length > 0) && (i === stringLength - 1)) {
+            return false
+          } else if (arrayOpenbracketsQueue.length > 0) {
+            continue
+          }
+        }
+          break
+      }
+    }
+  }
+}
+
+console.log(`The given string of brackets are balanced ${validateBalancedBrackets(validateBracketString)}`)
