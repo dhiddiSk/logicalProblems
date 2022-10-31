aa4efe6cf3619182214750e7611684aa0cf86b2f
saikrishna <saikrishna.4369@gmail.com>
2022-10-31 13:23:27 +0100

    converted to ts from js

---
+++ javaScript/jsSource/orginalSentence.ts
@@ -0,0 +1,27 @@
+/*
+This problem was asked by Microsoft.
+Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list.
+If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
+For example, given the set of words 'quick', 'brown', 'the', 'fox', and the string "thequickbrownfox", you should return ['the', 'quick', 'brown', 'fox'].
+Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+*/
+
+const orginalSentence = function (sentence: string, ...words: string[]): string[] {
+  const orderedWords: string[] = []
+  let start = 0; let end = 1
+  const sentenceLength: number = sentence.length
+  for (let i = 0; i < sentenceLength; i++) {
+    const tempStringHolder = sentence.substring(start, end)
+    if (!(words.includes(tempStringHolder))) {
+      end++
+      continue
+    } else if (words.includes(tempStringHolder)) {
+      orderedWords.push(tempStringHolder)
+      start = end
+      end = end + 1
+    }
+  }
+  return orderedWords
+}
+
+console.log(orginalSentence('bedbathandbeyond', 'bed', 'bath', 'bedbath', 'and', 'beyond'))
