5ce31059d87ba16f2f277e139e2f2d53f20b3a1b
saikrishna <saikrishna.4369@gmail.com>
2022-11-03 10:17:36 +0100

    update

--- javaScript/test/twoSumArray.test.ts
+++ javaScript/test/twoSumArray.test.ts
@@ -1,10 +1,10 @@
 'use strict'
-import { twoSum as twoSumArray } from '../jsSource/twoSumArray'
+import { twoSumArray as twoSum } from '../jsSource/twoSumArray'
 
 describe('two sum array tests with different inputs', () => {
   test('test with different inputs', () => {
-    expect(twoSumArray([1, 2, 3, 4], 7)).toEqual([2, 3])
-    expect(twoSumArray([2, 0], 2)).toEqual([0, 1])
-    expect(twoSumArray([1, -2, -1, 3], 4)).toEqual([0, 3])
+    expect(twoSum([1, 2, 3, 4], 7)).toEqual([2, 3])
+    expect(twoSum([2, 0], 2)).toEqual([0, 1])
+    expect(twoSum([1, -2, -1, 3], 4)).toEqual([0, 3])
   })
 })
