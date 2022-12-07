4ecf4bf8b2760af5dbefcd50e0ea47c563bc81ab
saikrishna <saikrishna.4369@gmail.com>
2022-12-07 23:44:07 +0530

    update

---
+++ javaScript/jsSource/longestCommonPrefix.ts
@@ -0,0 +1,8 @@
+function longestCommonPrefix() {
+  const temp = ["flower","flow","flight"];
+    for(const arrayItem of Object.values(temp)){
+        console.log(arrayItem);
+    }
+};
+
+longestCommonPrefix();
