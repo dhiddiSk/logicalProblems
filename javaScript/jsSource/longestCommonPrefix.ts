5e8cea795dfa57f0333d64a4629910fe7579ddc4
saikrishna <saikrishna.4369@gmail.com>
2022-12-08 16:19:01 +0530

    longestCommonPrefix solution

--- javaScript/jsSource/longestCommonPrefix.ts
+++ javaScript/jsSource/longestCommonPrefix.ts
@@ -1,8 +1,19 @@
-function longestCommonPrefix() {
-  const temp = ["flower","flow","flight"];
-    for(const arrayItem of Object.values(temp)){
-        console.log(arrayItem);
-    }
-};
+function longestCommonPrefix(strs: string[]): string {
+    
+  let prefix = strs[0];
+  for(const arrayItem of Object.values(strs)){
 
-longestCommonPrefix();
+  if (strs.length === 1) {
+      return strs[0];
+  }
+  for(let i =1; i<strs.length; i++){
+      while(!strs[i].startsWith(prefix)){
+          prefix = prefix.slice(0, -1);
+      }
+      if(prefix === ''){
+          return prefix;
+      }
+  }
+  return prefix;
+  }
+};
