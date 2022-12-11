a2ca55b09c09ff4bd6fee60c58b616a1e0b99628
saikrishna <saikrishna.4369@gmail.com>
2022-12-11 13:41:45 +0530

    removeDuplicates problem

---
+++ javaScript/jsSource/removeDuplicates.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let insertIndex = 1;
+    let arrayLength = nums.length;
+    for(let i=1; i<arrayLength; i++){
+        if(nums[i-1] != nums[i]){
+           nums[insertIndex] = nums[i];
+           insertIndex++; 
+        }
+    }
+    return insertIndex;
+};
