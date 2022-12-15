487aa29167c52156329cfd2eb12f72b8a0cedbe7
saikrishna <saikrishna.4369@gmail.com>
2022-12-15 21:30:05 +0530

    update

---
+++ javaScript/jsSource/lengthOfLastWord.ts
@@ -0,0 +1,5 @@
+function lengthOfLastWord(s: string): number {
+    const words = s.split(" ").filter((v) => v)
+    const maybeLast = words[words.length - 1]
+    return maybeLast ? maybeLast.length : 0;
+};
