dc1895fb504335d9f4900b6715e1065e60180e03
saikrishna <saikrishna.4369@gmail.com>
2023-02-27 19:36:59 +0100

    largest number logical problem

---
+++ java/src/main/java/LargestNumber.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumber {
+
+	public String largestNumber(int[] input) {
+		String[] numsToStrings = new String[input.length];
+	    for(int i=0;i<input.length;i++) numsToStrings[i] = String.valueOf(input[i]);
+
+	    Comparator<String> comp = new Comparator<String>(){
+	        @Override
+	        public int compare(String s1, String s2){
+	            String str1 = s1 + s2;
+	            String str2 = s2 + s1;
+	            return str2.compareTo(str1);
+	        }
+	    };
+
+	    Arrays.sort(numsToStrings, comp);
+
+	    StringBuilder sb = new StringBuilder();
+
+	    for(String s:numsToStrings){
+	        sb.append(s);
+	    }
+	    return sb.toString();
+
+		}
+}
